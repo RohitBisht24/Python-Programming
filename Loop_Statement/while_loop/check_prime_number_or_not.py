54e01ec5a6ef69de1cedc3c689f18d62ac52f409
Rohit Singh Bisht <rohitsinghbisht568@gmail.com>
2026-07-20 02:55:30 +0530

    Add script to check if a number is prime using a while loop

---
+++ Loop_Statement/while_loop/check_prime_number_or_not.py
@@ -0,0 +1,19 @@
+# check Prime Number or not
+
+num = int(input("Enter the Number : "))
+
+i = 2
+is_prime = True
+
+if (num == 1):
+    print("Non Prime Number.")
+else:
+    while (i < num):
+        if num % i == 0:
+            is_prime = False
+        i += 1
+
+    if is_prime == True:
+        print("Prime Number.")
+    else:
+        print("Non Prime Number.")
