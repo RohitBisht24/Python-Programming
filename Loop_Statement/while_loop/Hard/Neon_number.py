fe0bd6830cba525fea515f786226ad103355268f
Rohit Singh Bisht <rohitsinghbisht568@gmail.com>
2026-08-08 01:15:19 +0530

    edit Neon Number.py file currect the print function string

--- Loop_Statement/while_loop/Hard/Neon_number.py
+++ Loop_Statement/while_loop/Hard/Neon_number.py
@@ -1,14 +1,14 @@
 # check whether a number is Neon number.
+
 num = int(input("Enter the Number : "))
 square = num**2
 sum = 0
 while square > 0:
-    # square = num**2
     LD = square % 10
     sum += LD
     square //= 10
 
 if num == sum:
-    print("same ")
+    print(f"{num} is a Neon Number.")
 else:
-    print("not a same")
+    print(f"{num} is not a Neon Number.")
